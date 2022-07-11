340679dd7ecb993c0643cd16a953a2113f29b7e8
timmoh-king <timothykariuki43@gmail.com>
2022-07-11 17:17:09 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+/**
+ * @Author: Your name
+ * @Date:   2022-07-10 22:54:23
+ * @Last Modified by:   Your name
+ * @Last Modified time: 2022-07-11 17:16:33
+ */
+import "./App.css";
+import Movies from "./components/Movies";
+
+function App() {
+  return (
+    <div className="App">
+      <Movies />
+    </div>
+  );
+}
+
+export default App;
